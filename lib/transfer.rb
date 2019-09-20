0d2fb4c61d81fd954da520b73b8f7d8d00b2c0c8
Joseph Sesti <sestijoey63@gmail.com>
2019-09-20 17:16:20 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,10 +14,10 @@ class Transfer
   end
   
   def execute_transaction
-    if valid? && sender.balance > amount && 
+    if valid? && sender.balance > amount && @status == "pending"
       sender.balance -= amount
       receiver.balance += amount
-      self.status = "complete"
+      @status = "complete"
     else
       @status = "rejected"
       "Transaction rejected. Please check your account balance."
